cd43fc73cc984e00c6061ae639344b8e242be5c3
Riyadulislamratul <riadul1810833319@gmail.com>
2026-04-19 23:28:11 +0600

    Header and Footer added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,20 @@
-import React, { useRef, useState } from 'react'
-import Header from './Header'
-import Video from '../assets/banner.mp4'
-
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import Video from "../assets/banner.mp4";
 const Banner = () => {
-  const videoRef = useRef(null)
-  const [isPaused, setIsPaused] = useState(false)
+  const videoRef = useRef(null);
+  const [isPaused, setIsPaused] = useState(false);
 
   const togglePlay = () => {
     if (videoRef.current) {
       if (isPaused) {
-        videoRef.current.play()
+        videoRef.current.play();
       } else {
-        videoRef.current.pause()
+        videoRef.current.pause();
       }
-      setIsPaused(!isPaused)
+      setIsPaused(!isPaused);
     }
-  }
+  };
 
   return (
     <div className="relative">
@@ -34,10 +33,9 @@ const Banner = () => {
         </video>
 
         {/* Overlay content */}
-        <div className="absolute inset-0 flex items-center justify-center text-white">
-          <h1 className="text-5xl font-bold">
-            We Find The Best Tours For You
-          </h1>
+        <div className="absolute inset-0 flex items-center flex-col justify-center text-white">
+          <h1 className="text-5xl font-bold font-volkhov text-black-heading">We Find The Best Tours For You</h1>
+          <p className="text-[16px] leading-6.25 font-mulish pt-5.5 w-[569px] text-center text-black-heading">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
         </div>
 
         {/* Pause Button */}
@@ -45,11 +43,11 @@ const Banner = () => {
           onClick={togglePlay}
           className="absolute bottom-10 right-10 bg-white/80 hover:bg-white text-black px-4 py-2 rounded-lg"
         >
-          {isPaused ? 'Play' : 'Pause'}
+          {isPaused ? "Play" : "Pause"}
         </button>
       </div>
     </div>
   );
-}
+};
 
-export default Banner
+export default Banner;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router";
+import logoDark from "../assets/logoblack.png";
+import logoWhite from "../assets/logo.png";
+
 export default function Header({ isHome }) {
   return (
     <header
@@ -6,15 +10,36 @@ export default function Header({ isHome }) {
         ${isHome ? "absolute top-0 left-0 bg-transparent" : "bg-white shadow"}
       `}
     >
-      <div className="max-w-7xl mx-auto flex justify-between items-center py-4 px-6">
-        <h1 className={`${isHome ? "text-white" : "text-black"} font-bold`}>
-          Tour Guide
-        </h1>
+      <div className=" flex justify-between items-center py-8.5 px-10">
+        <Link to="/" className="h-5.5 w-45">
+          <img
+            src={isHome ? logoWhite : logoDark}
+            alt="tour guide logo"
+            className="h-full w-full"
+          />
+        </Link>
 
-        <nav className="flex gap-6">
-          <a className={isHome ? "text-white" : "text-black"}>Home</a>
-          <a className={isHome ? "text-white" : "text-black"}>About</a>
-          <a className={isHome ? "text-white" : "text-black"}>Packages</a>
+        <nav className="flex gap-7.5 font-mulish text-[15px] items-center font-500">
+          <Link to="/" className={isHome ? "text-white" : "text-dark-80"}>
+            Home
+          </Link>
+          <Link to="/about" className={isHome ? "text-white" : "text-dark-80"}>
+            About Us
+          </Link>
+          <Link to="/packages" className={isHome ? "text-white" : "text-dark-80"}>
+            Our Packages
+          </Link>
+          <Link to="/packages" className={isHome ? "text-white" : "text-dark-80"}>
+            Popular Destinations
+          </Link>
+          <Link to="/packages" className={isHome ? "text-white" : "text-dark-80"}>
+            Help
+          </Link>
+          <div>
+            <button className="py-3.5 px-10.75 bg-amber-300 font-mulish font-extrabold text-[16px] rounded-[3px]">
+              Sign in
+            </button>
+          </div>
         </nav>
       </div>
     </header>
